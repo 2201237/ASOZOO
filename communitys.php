1cf5d7be4bec59bb1b43c58aa7db14f4e96c3ed0
2201237 <2201237@s.asojuku.ac.jp>
2024-05-23 10:14:07 +0900

    Update communitys.php

--- communitys.php
+++ communitys.php
@@ -16,7 +16,7 @@
         echo '</form>';
         foreach($pdo->query('select * from community') as $row){
             $id=$row['community_id'];
-            echo '<p><img alt="image" src="../img/', $row['jpg'], '.jpg" height="100" width="120"></p>';
+            echo '<p><img alt="image" src="img/', $row['jpg'], '.jpg" height="100" width="120"></p>';
             echo '<h3><a href="community_top.php?id=', $id, '">', $row['community_name'], '</a></h3>';
         }
     ?>

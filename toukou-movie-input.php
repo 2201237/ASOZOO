9920451535d321f76a9abb9f9f36352e15a38e79
kawamoto0719 <148738656+kawamoto0719@users.noreply.github.com>
2024-05-27 10:30:49 +0900

    toukou

---
+++ toukou-movie-input.php
@@ -0,0 +1,37 @@
+<?php require "header.php" ?>
+<body>
+    <center>
+<form enctype="multipart/form-data"  action="toukou-image-output.php" method="POST">
+    <input type="hidden" name="name" value="value" />
+
+    <p>
+    <label>動画</label>
+    <input name="user_file_name" type="file" />
+    </p>
+
+    <p>
+    <label>タイトル</label>
+    <input type="text" name="title">
+    </p>
+
+    <p>
+    <label>コメント</label>
+    <input type="text" name="content">
+    </p>
+
+    <p>
+    <label>タグ</label>
+    <input type="text" name="tag">
+    </p>
+
+
+
+
+<input type="submit" value="投稿" />
+</form>
+</center>
+</body>
+</html>
+
+
+
